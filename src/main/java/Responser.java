c5c81aff09e1e4cd0ff5d3865c6b25265b5570fe
offMomySon <ohji100629@naver.com>
2021-11-04 22:32:20 +0900

    base_sned_file

--- src/main/java/Responser.java
+++ src/main/java/Responser.java
@@ -1,8 +1,10 @@
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import java.util.Date;
 
 public class Responser implements Runnable{
+    private static final String CR = "\r";
+    private static final String LF = "\n";
     private static final String FINISH_READ_REQUEST_MESSAGE = "\r\n\r\n";
 
     private final InputStream inputStream;
@@ -16,17 +18,100 @@ public class Responser implements Runnable{
     @Override
     public void run() {
         try {
-            getRequestMessage();
-            sendResponseMessage();
+            String filePath = readRequestMessage();
+            sendTempResponseMsg(filePath);
+//            sendResponseMessage();
         } catch (IOException e) {
             System.out.println("some");
             e.printStackTrace();
         }
     }
 
+    private void sendTempResponseMsg(String path) throws IOException {
+        File file = new File("src/main/resources" + path);
+
+        if(file.exists()){
+            System.out.println("file exist");
+            if(file.isDirectory()){
+                System.out.println("this is file");
+
+            }else if(file.isFile()){
+                System.out.println("this is file.");
+
+                try {
+                    System.out.println("star make header");
+
+                    StringBuilder responseBuilder = new StringBuilder();
+                    responseBuilder.append("HTTP/1.1 200 OK \r\n");
+                    responseBuilder.append("Content-Length : "+ file.length() +"\r\n");
+                    responseBuilder.append("Content-Type: " + getContentType(path) + "\r\n");
+                    responseBuilder.append("Date: " + new Date() + "\r\n");
+                    responseBuilder.append("Server: jihun server 1.0 \r\n");
+                    responseBuilder.append("\r\n");
+
+                    outputStream.write(responseBuilder.toString().getBytes(StandardCharsets.UTF_8));
+                    System.out.println("end make header");
+
+                    readFile(new FileInputStream(file), outputStream );
+
+//                    outputStream.write("\r\n".getBytes(StandardCharsets.UTF_8));
+                    outputStream.flush();
+                    System.out.println("flushed..");
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }else{
+            System.out.println("file not exist");
+        }
+    }
+
+    private void readFile(InputStream fileInputStream, OutputStream outputStream){
+        System.out.println("star readFile");
+
+        try {
+            byte[] buffer = new byte[8192];
+            int readNo = -1;
+
+            while((readNo = fileInputStream.read(buffer)) != -1) {
+                System.out.println("loop read..");
+                outputStream.write(buffer,0,readNo);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("end readFile..");
+    }
+
+    private String getContentType(String path){
+        if (path.endsWith(".html") || path.endsWith(".htm"))
+            return "text/html";
+        else if (path.endsWith(".txt") || path.endsWith(".java"))
+            return "text/plain";
+        else if (path.endsWith(".gif"))
+            return "image/gif";
+        else if (path.endsWith(".png"))
+            return "image/png";
+        else if (path.endsWith(".class"))
+            return "application/octet-stream";
+        else if (path.endsWith(".jpg") || path.endsWith(".jpeg"))
+            return "image/jpeg";
+        else if (path.endsWith(".mpeg"))
+            return "video/mpeg";
+        else if (path.endsWith(".ts"))
+            return "video/MP2T";
+        else if (path.endsWith(".avi"))
+            return "video/x-msvideo";
+        else
+            return "text/plain";
+    }
+
     private void sendResponseMessage() throws IOException {
+
         outputStream.write("HTTP/1.1 200 OK \r\n".getBytes(StandardCharsets.UTF_8));
         outputStream.write("Content-Type: text/html;charset=utf-8\r\n".getBytes(StandardCharsets.UTF_8));
+
         byte[] body = "Hello World".getBytes(StandardCharsets.UTF_8);
         String lenMessage = "Content-Length: " + body.length + "\r\n";
         outputStream.write(lenMessage.getBytes(StandardCharsets.UTF_8));
@@ -37,18 +122,31 @@ public class Responser implements Runnable{
         outputStream.flush();
     }
 
-    private void getRequestMessage() throws IOException {
+    private String readRequestMessage() throws IOException {
         byte[] buffer = new byte[8192];
         int readNo = -1;
-        StringBuilder sb = new StringBuilder();
+        StringBuilder httpRequestMessage = new StringBuilder();
 
         while((readNo = inputStream.read(buffer)) != -1 ) {
-            sb.append(new String(buffer, 0, readNo, StandardCharsets.UTF_8));
+            httpRequestMessage.append(new String(buffer, 0, readNo, StandardCharsets.UTF_8));
+            System.out.println("read result\n");
+            System.out.println(httpRequestMessage.toString());
 
-            if(sb.toString().contains(FINISH_READ_REQUEST_MESSAGE)){
+            if(httpRequestMessage.toString().contains(FINISH_READ_REQUEST_MESSAGE)){
                 System.out.println("finish read http request message.");
                 break;
             }
         }
+
+        return getPath(httpRequestMessage.toString());
+    }
+
+    private String getPath(String httpRequestMessage){
+        String startLine = httpRequestMessage.split(CR + LF, 2)[0];
+        System.out.println(startLine);
+        String path = startLine.split(" ")[1];
+        System.out.println(path);
+
+        return path;
     }
 }
---
+++ src/main/resources/Server.java
@@ -0,0 +1,52 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+
+public class Server {
+    private static final Socket UNBOUNDED = null;
+
+    private final ServerSocket serverSocket = createServerSocket();
+    private final ExecutorService executorService = ForkJoinPool.commonPool();
+
+    public ServerSocket createServerSocket() {
+        try {
+            return new ServerSocket(5001);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void start(){
+        Socket socket = UNBOUNDED;
+        try {
+            while(true){
+                System.out.println("waiting.. request");
+                socket = serverSocket.accept();
+                System.out.println("accept.. request");
+                System.out.printf("New Client Connect! Connected IP : %s, Port : %d\n", socket.getInetAddress(), socket.getPort());
+
+                executorService.submit(new Responser(socket.getInputStream(), socket.getOutputStream()));
+
+                socket = UNBOUNDED;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            try {
+                if(socket != null) socket.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+}
