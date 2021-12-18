b7877c620ae47e2122f7a71b42d7a162c6d48552
jihun_oh <ohji100629@naver.com>
2021-12-18 15:30:53 +0900

    ip download rate check, not finished

--- src/main/java/response/message/content/WelcomePageMessage.java
+++ src/main/java/response/message/content/WelcomePageMessage.java
@@ -9,34 +9,34 @@ import response.message.sender.Message;
 
 @Slf4j
 public class WelcomePageMessage extends Message {
-  private final ConfigManager configManager;
+    private final ConfigManager configManager;
 
-  public WelcomePageMessage(ConfigManager configManager) {
-    this.configManager = configManager;
-  }
+    public WelcomePageMessage(ConfigManager configManager) {
+        this.configManager = configManager;
+    }
 
-  @Override
-  protected String getContentType() {
-    return "text/html";
-  }
+    @Override
+    protected String getContentType() {
+        return "text/html";
+    }
 
-  @Override
-  protected String getContent() {
-    log.info("Start to response welcome, config");
+    @Override
+    protected String getContent() {
+        log.info("Start to response welcome, config");
 
-    ThreadConfig threadConfig = configManager.getThreadConfig();
-    BasicConfig basicConfig = configManager.getBasicConfig();
-    DownloadConfig downloadConfig = configManager.getDownloadConfig();
+        ThreadConfig threadConfig = configManager.getThreadConfig();
+        BasicConfig basicConfig = configManager.getBasicConfig();
+        DownloadConfig downloadConfig = configManager.getDownloadConfig();
 
-    content.append("Server port : ").append(basicConfig.getPort()).append("</br>");
-    content.append("welcome page path : ").append(basicConfig.getWelcomePageURI()).append("</br>");
-    content.append("usable thread count : ").append(threadConfig.getUsableThreadCount()).append("</br>");
-    content.append("waitable thread count : ").append(threadConfig.getWaitableThreadCount()).append("</br>");
-    content.append("download path : ").append(downloadConfig.getRootPath()).append("</br>");
-    content.append("download count : ").append(downloadConfig.getCount()).append("</br>");
-    content.append("download period : ").append(downloadConfig.getPeriod()).append("</br>");
-    content.append("restricted file extension : ").append(downloadConfig.getRestrictedFileExtension().toString()).append("</br>");
+        content.append("Server port : ").append(basicConfig.getPort()).append("</br>");
+        content.append("welcome page path : ").append(basicConfig.getWelcomePageURI()).append("</br>");
+        content.append("usable thread count : ").append(threadConfig.getUsableThreadCount()).append("</br>");
+        content.append("waitable thread count : ").append(threadConfig.getWaitableThreadCount()).append("</br>");
+        content.append("download path : ").append(downloadConfig.getRootPath()).append("</br>");
+        content.append("download count : ").append(downloadConfig.getDownloadRate().getCount()).append("</br>");
+        content.append("download period : ").append(downloadConfig.getDownloadRate().getPeriod()).append("</br>");
+        content.append("restricted file extension : ").append(downloadConfig.getRestrictedFileExtension().toString()).append("</br>");
 
-    return content.toString();
-  }
+        return content.toString();
+    }
 }
--- src/test/java/config/server/download/DownloadConfigTest.java
+++ src/test/java/config/server/download/DownloadConfigTest.java
@@ -48,7 +48,7 @@ class DownloadConfigTest {
         FileExtension fileExtension = FileExtension.parse(extension);
 
         //when
-        boolean actual = ConfigManager.getInstance().getDownloadConfig().containsRestrictedFileExtensionAtIp(hostAddress, fileExtension);
+        boolean actual = ConfigManager.getInstance().getDownloadConfig().containsRestrictedFileExtensionAtHostAddress(hostAddress, fileExtension);
 
         //then
         Assertions.assertThat(actual).isTrue();
@@ -60,9 +60,37 @@ class DownloadConfigTest {
     void containsIpAddress(String hostAddress) {
         //given
         //when
-        boolean actual = ConfigManager.getInstance().getDownloadConfig().containsIpAddress(hostAddress);
+        boolean actual = ConfigManager.getInstance().getDownloadConfig().containsHostAddressAtRestrictedFileExtension(hostAddress);
 
         //then
         Assertions.assertThat(actual).isTrue();
+    }
+
+    @DisplayName("특정 hostAddress 가 download config 들 중에 포함되어있으면 true 를 반환해야 합니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"192.168.0.1", "192.168.0.2"})
+    void containsHostAddressAtSpecificDownloadConfig(String hostAddress) {
+        //given
+        DownloadConfig downloadConfig = ConfigManager.getInstance().getDownloadConfig();
+
+        //when
+        boolean actual = downloadConfig.containsHostAddressAtSpecificDownloadConfig(hostAddress);
+
+        //then
+        Assertions.assertThat(actual).isTrue();
+    }
+
+    @DisplayName("특정 hostAddress 가 download config 들 중에 포함되지 않는다면 false 를 반환해야 합니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"192.168.0.3", "192.168.0.4"})
+    void not_containsHostAddressAtSpecificDownloadConfig(String hostAddress) {
+        //given
+        DownloadConfig downloadConfig = ConfigManager.getInstance().getDownloadConfig();
+
+        //when
+        boolean actual = downloadConfig.containsHostAddressAtSpecificDownloadConfig(hostAddress);
+
+        //then
+        Assertions.assertThat(actual).isFalse();
     }
 }
